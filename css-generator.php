0faa053a2d0559a17a7c45469d2f231ebfa369e1
wac <daisy.bruneau@epitech.eu>
2019-12-11 13:22:41 +0100

    .

---
+++ css-generator.php
@@ -0,0 +1,36 @@
+<?php
+
+function sprite($files = array(), $go = '')
+{
+    $files_template = array();
+    $path = '/home/wac/daisyB-repo/css_generator';
+
+
+    is_dir($filename)
+    {
+        
+    }
+
+    foreach ($files as $file)
+    {
+        list($w_idth, $h_eight, $t_ype) = getimagesize($file);
+    }
+    
+    foreach ($files_template as $file)
+    {
+        if ()
+        {
+
+        }
+
+    //$handle = fopen('somefile.txt', 'r');
+
+    //fclose($handle);
+        
+    }
+}
+
+
+//fopen
+//fclose
+
