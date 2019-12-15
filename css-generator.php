d576b37cb5cae61dcae474c4c7544a19e01a0e07
wac <daisy.bruneau@epitech.eu>
2019-12-15 19:30:50 +0100

    .

--- css-generator.php
+++ css-generator.php
@@ -1,25 +1,24 @@
 <?php
 
-function getDirContents($dir)
+function getDirContents($path)
 {
 
-    if (file_exists($dir) && is_dir($dir) ) {
-        $handle = opendir($dir);
-        print $dir .":" .PHP_EOL;
+    if (file_exists($path) && is_dir($path) ) 
+    {
+        $handle = opendir($path);
+        print_r($path .":" .PHP_EOL);
 
-        if (!$handle) return array();
-        {
+
             while ($entry = readdir($handle)) 
             {
                 if (is_file($entry)) 
                 {
-                    $filelist = glob($handle . "*.png");
+                    $filepng = glob(/*$handle .*/"*.png");
                     
-                    foreach ($filelist as $file_list)
+                    foreach ($filepng as $file_png)
                     {
-                        $file_path = '.' .$file_list;
-                        $filelist = $entry;
-                        $filelist = array();
+                        $file_path = '.' .$file_png;
+                        $file_png = array($entry);
                         $file_ext = pathinfo($file_path, PATHINFO_EXTENSION);
                     }
 
@@ -51,9 +50,7 @@ function sprite($files = array(), $path_gen = '', $imgheight = 100, $imgwidth = 
     $height = $imgheight;
     $img = imagecreatetruecolor($width, $height);
     $bgc = imagecolorallocate($img, 255, 255, 255);
-    //imagefill($img, 0, 0, $background);
-    //imagealphablending($img, false);
-    //imagesavealpha($img, true);
+
 
     //ajout des images
     $position = 0;
@@ -68,23 +65,25 @@ function sprite($files = array(), $path_gen = '', $imgheight = 100, $imgwidth = 
         {
             die('ERREUR : Cette image n\'est pas au format PNG');
         }
+        
+        imagecopy($img, $tmp, $position, 0, 0, 0, $imgwidth, $imgheight);
+        $position += ($spacing + $imgwidth);
+        
+        //affiche l'image
+        //if (empty($path_gen))
+        //{
+            //header('Content-Type: image/png');
+            imagepng($img, 'home/wac/daisyB-repo/css_generator/sprite.png');
+            //}
+            //else 
+            //{
+                //imagepng($img, $path_gen);
+                //}
+                //imagedestroy($tmp);
+                
     }
-
-    imagecopy($img, $tmp, $position, 0, 0, 0, $imgwidth, $imgheight);
-    $position += ($spacing + $imgwidth);
-    
-    //affiche l'image
-    if (empty($path_gen))
-    {
-        header('Content-Type: image/png');
-        imagepng($img, 'sprite.png');
-    }
-    else 
-    {
-        imagepng($img, $path_gen);
-    }
-    imagedestroy($tmp);
 
 }
 
 getDirContents('/home/wac/daisyB-repo/css_generator');
+sprite(array('mario.png', 'fortnite.png'), 'home/wac/daisyB-repo/css_generator/');
